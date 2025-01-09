434963b230136ed7c395e112e0623407eb74db20
CHr <christoph.trischler@t-online.de>
2025-01-09 14:46:13 +0100

    bottombar

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text("Studi app")),
-        actions: [
-          IconButton(onPressed: () => (), icon: const Icon(Icons.verified_user))
-        ],
-      ),
-      key: _scaffoldKey,
-      drawer: Sidebar(),
-    ));
+            appBar: TopBar(),
+            key: _scaffoldKey,
+            drawer: Sidebar(),
+            bottomNavigationBar: BottomBar()));
+  }
+}
+
+class BottomBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(items: [
+      BottomNavigationBarItem(icon: Icon(Icons.music_note), label: "Hy"),
+      BottomNavigationBarItem(icon: Icon(Icons.music_note), label: "ho")
+    ]);
+  }
+}
+
+class TopBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size(15, 100);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Center(child: Text("Studi app")),
+      actions: [
+        IconButton(onPressed: () => (), icon: const Icon(Icons.account_circle))
+      ],
+    );
   }
 }
 
